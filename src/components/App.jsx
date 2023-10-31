48e6c319dbf63cb5245831323581e41204925c9a
Cotterell Anna <cotterellanna@gmail.com>
2023-10-31 15:11:51 +0100

    alt added

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -24,7 +24,7 @@ function App(){
                     {
                         textColor:"text-light",
                         bgImg:"/images/apple_watch_series_9.jpeg",
-                        heading:<img src='/images/series9_watch_heading.png' />,
+                        heading:<img src='/images/series9_watch_heading.png' alt="" />,
                         subHeading:<h3>Smarter. Brighter. Mightier.</h3>
                     }
                 ]}
@@ -37,38 +37,38 @@ function App(){
                         textColor:"text-light",
                         bgImg:"/images/ipad_pro.jpeg",
                         heading:"iPad Pro",
-                        subHeading:["Supercharged by ", <img src='/images/m2_chip.png' />]
+                        subHeading:["Supercharged by ", <img src='/images/m2_chip.png' alt="" />]
                     },
                     {
                         alignItems:"align-items-end",
                         textColor:"text-dark",
                         bgImg:"/images/vision_pro.jpeg",
-                        heading:<img src='/images/vision_pro-text.png' />,
+                        heading:<img src='/images/vision_pro-text.png' alt="" />,
                         subHeading:<p>Welcome to the era of spatial computing.</p>
                     },
                     {
                         textColor:"text-dark",
                         bgImg:"/images/macbook_air.jpeg",
                         heading:'MacBook Air 15"',
-                        subHeading:"Impressively big. Impossibly thin."
+                        subHeading: <p>Impressively big. Impossibly thin.</p>
                     },
                     {
                         textColor:"text-light",
                         bgImg:"/images/airpods_pro.jpeg",
                         heading:"Airpods Pro",
-                        subHeading:"Adaptive Audio. Now playing."
+                        subHeading:<p>Adaptive Audio. Now playing.</p>
                     },
                     {
                         textColor:"text-dark",
                         bgImg:"/images/trade_in.jpeg",
-                        heading:<img src="/images/trade_in_text.png" />,
-                        subHeading:"Get $200-$650 in credit when you trade in iPhone 11 or higher."
+                        heading:<img src="/images/trade_in_text.png" alt="" />,
+                        subHeading:<p>Get $200-$650 in credit when you trade in iPhone 11 or higher.</p>
                     },
                     {
                         textColor:"text-dark",
                         bgImg:"/images/apple_card.jpeg",
-                        heading:<img src="/images/card_text.png" />,
-                        subHeading:"Get up to 3% Daily cash back with every purchase."
+                        heading:<img src="/images/card_text.png" alt="" />,
+                        subHeading:<p>Get up to 3% Daily cash back with every purchase.</p>
                     }
                 ]}
              />
--- src/components/Unit.jsx
+++ src/components/Unit.jsx
@@ -15,7 +15,7 @@ function Unit(props){
             <div className={`d-flex justify-content-center ${props.alignItems}`} style={bgStyle}>
                 <div className={`pt-5 px-5 mb-5 text-center ${props.textColor}`}>
                     <h2 className="mx-auto" style={{fontSize:"3.8em"}}>{props.heading}</h2>
-                    <div style={{fontSize:"1.5em"}}>{props.subHeading}</div>
+                    <div style={{fontSize:"1.5em", maxWidth:"32rem"}}>{props.subHeading}</div>
                     <div className="container">
                         <a className="fs-5 text-decoration-none me-4" href={props.learnMoreLink}>Learn more <i className="bi bi-chevron-right" style={{fontSize:"1em", lineHeight:"1.2"}}></i></a>
                         <a className="fs-5 text-decoration-none" href={props.buyLink}>Buy <i className="bi bi-chevron-right" style={{fontSize:"1em", lineHeight:"1.2"}}></i></a>
